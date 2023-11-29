d20da01149e56598cc3f00c589423769fff92ab1
plant-12 <tanvisinghalusa@gmail.com>
2023-11-29 17:01:01 -0500

    updated memory files

--- memory.js
+++ memory.js
@@ -1,27 +1,51 @@
 const cards = document.querySelectorAll(".card");
-
 let matched = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
+let timer;
 
-function flipCard({target: clickedCard}) {
-    if(cardOne !== clickedCard && !disableDeck) {
+const timerDisplay = document.getElementById("timer");
+
+function flipCard({ target: clickedCard }) {
+    if (cardOne !== clickedCard && !disableDeck) {
         clickedCard.classList.add("flip");
-        if(!cardOne) {
-            return cardOne = clickedCard;
+        if (!cardOne) {
+            cardOne = clickedCard;
+            if (matched === 0) {
+                startTimer();
+            }
+            return;
         }
         cardTwo = clickedCard;
         disableDeck = true;
         let cardOneImg = cardOne.querySelector(".back-view img").src,
-        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+            cardTwoImg = cardTwo.querySelector(".back-view img").src;
         matchCards(cardOneImg, cardTwoImg);
     }
 }
 
+function startTimer() {
+    let timeRemaining = 60; // 60 seconds
+
+    timer = setInterval(() => {
+        timeRemaining--;
+
+        if (timeRemaining <= 0) {
+            clearInterval(timer);
+            gameOver();
+        }
+
+        // Display the time remaining if needed
+        // You can update a timer display element here if you have one
+        // Update the timer display
+        timerDisplay.textContent = `${Math.floor(timeRemaining / 60)}:${(timeRemaining % 60).toString().padStart(2, '0')}`;
+    }, 1000);
+}
+
 function matchCards(img1, img2) {
-    if(img1 === img2) {
+    if (img1 === img2) {
         matched++;
-        if(matched == 8) {
+        if (matched === 8) {
             setTimeout(() => {
                 return shuffleCard();
             }, 1000);
@@ -29,19 +53,20 @@ function matchCards(img1, img2) {
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
         cardOne = cardTwo = "";
-        return disableDeck = false;
-    }
-    setTimeout(() => {
-        cardOne.classList.add("shake");
-        cardTwo.classList.add("shake");
-    }, 400);
-
-    setTimeout(() => {
-        cardOne.classList.remove("shake", "flip");
-        cardTwo.classList.remove("shake", "flip");
-        cardOne = cardTwo = "";
         disableDeck = false;
-    }, 1200);
+    } else {
+        setTimeout(() => {
+            cardOne.classList.add("shake");
+            cardTwo.classList.add("shake");
+        }, 400);
+
+        setTimeout(() => {
+            cardOne.classList.remove("shake", "flip");
+            cardTwo.classList.remove("shake", "flip");
+            cardOne = cardTwo = "";
+            disableDeck = false;
+        }, 1200);
+    }
 }
 
 function shuffleCard() {
@@ -49,7 +74,7 @@ function shuffleCard() {
     disableDeck = false;
     cardOne = cardTwo = "";
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
     cards.forEach((card, i) => {
         card.classList.remove("flip");
         let imgTag = card.querySelector(".back-view img");
@@ -58,8 +83,18 @@ function shuffleCard() {
     });
 }
 
+function gameOver() {
+    alert("Game Over! You ran out of time. Please try again.");
+    resetGame();
+}
+
+function resetGame() {
+    clearInterval(timer);
+    shuffleCard();
+}
+
 shuffleCard();
-    
-cards.forEach(card => {
+
+cards.forEach((card) => {
     card.addEventListener("click", flipCard);
 });
