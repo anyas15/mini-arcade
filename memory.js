47532ebc72f34a1542e4666c5b4cef4c8ec6a5bf
plant-12 <tanvisinghalusa@gmail.com>
2023-12-04 00:59:14 -0500

    updated points system

--- memory.js
+++ memory.js
@@ -1,20 +1,15 @@
 const cards = document.querySelectorAll(".card");
+const scoreDisplay = document.getElementById("points"); // Updated to use the "points" div
 let matched = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
-let timer;
-
-const timerDisplay = document.getElementById("timer");
+let points = 0;
 
 function flipCard({ target: clickedCard }) {
     if (cardOne !== clickedCard && !disableDeck) {
         clickedCard.classList.add("flip");
         if (!cardOne) {
-            cardOne = clickedCard;
-            if (matched === 0) {
-                startTimer();
-            }
-            return;
+            return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
         disableDeck = true;
@@ -24,28 +19,11 @@ function flipCard({ target: clickedCard }) {
     }
 }
 
-function startTimer() {
-    let timeRemaining = 60; // 60 seconds
-
-    timer = setInterval(() => {
-        timeRemaining--;
-
-        if (timeRemaining <= 0) {
-            clearInterval(timer);
-            gameOver();
-        }
-
-        // Display the time remaining if needed
-        // You can update a timer display element here if you have one
-        // Update the timer display
-        timerDisplay.textContent = `${Math.floor(timeRemaining / 60)}:${(timeRemaining % 60).toString().padStart(2, '0')}`;
-    }, 1000);
-}
-
 function matchCards(img1, img2) {
     if (img1 === img2) {
         matched++;
-        if (matched === 8) {
+        if (matched == 8) {
+            updateScore(5); // Add 5 points when all cards are matched
             setTimeout(() => {
                 return shuffleCard();
             }, 1000);
@@ -53,20 +31,19 @@ function matchCards(img1, img2) {
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
         cardOne = cardTwo = "";
-        disableDeck = false;
-    } else {
-        setTimeout(() => {
-            cardOne.classList.add("shake");
-            cardTwo.classList.add("shake");
-        }, 400);
-
-        setTimeout(() => {
-            cardOne.classList.remove("shake", "flip");
-            cardTwo.classList.remove("shake", "flip");
-            cardOne = cardTwo = "";
-            disableDeck = false;
-        }, 1200);
+        return disableDeck = false;
     }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
 }
 
 function shuffleCard() {
@@ -74,7 +51,7 @@ function shuffleCard() {
     disableDeck = false;
     cardOne = cardTwo = "";
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
     cards.forEach((card, i) => {
         card.classList.remove("flip");
         let imgTag = card.querySelector(".back-view img");
@@ -83,18 +60,13 @@ function shuffleCard() {
     });
 }
 
-function gameOver() {
-    alert("Game Over! You ran out of time. Please try again.");
-    resetGame();
-}
-
-function resetGame() {
-    clearInterval(timer);
-    shuffleCard();
+function updateScore(amount) {
+    points += amount;
+    scoreDisplay.innerText = `Points: ${points}`;
 }
 
 shuffleCard();
 
-cards.forEach((card) => {
+cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
